0782954e767f7a66602d5fea5d19b6b4a054a8f2
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-05 12:24:03 +0500

    Update stg_rental.sql

--- sql/staging/stg_rental.sql
+++ sql/staging/stg_rental.sql
@@ -1,1 +1,11 @@
-
+create table stage.stg_rental(
+	rental_id integer,
+	rental_date timestamp,
+	inventory_id integer,
+	customer_id integer,
+	return_date timestamp,
+	staff_id integer,
+	last_update timestamp,
+	load_dts timestamp not null,
+	record_source varchar(50) not null
+);
