eb1a2d28a28308f000a7a219d7a0c514b5bfeb79
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-05 11:40:39 +0500

    Create staging table for customer data

--- sql/staging/stg_customer.sql
+++ sql/staging/stg_customer.sql
@@ -1,1 +1,14 @@
-
+create table stage.stg_customer(
+	customer_id 	int,
+	store_id 		integer,
+	first_name 		varchar(45),
+	last_name 		varchar(45),
+	email 			varchar(50),
+	address_id 		integer,
+	activebool 		boolean,
+	create_date 	date,
+	last_update 	timestamp,
+	active 			int4,
+	load_dts 		timestamp not null,
+	record_source 	varchar(50) not null
+);
