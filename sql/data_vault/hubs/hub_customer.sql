84a0c9429858dcd985e625d2c030d408b5986d44
i.nigina <i.nigina@decosystems.ru>
2026-01-07 11:27:59 +0500

    Changed keys

--- sql/data_vault/hubs/hub_customer.sql
+++ sql/data_vault/hubs/hub_customer.sql
@@ -1,6 +1,6 @@
 create table core.hub_customer (
     customer_hk   char(32) not null,
-    customer_id   integer not null,
+    customer_bk   integer not null,
     load_dts      timestamp not null,
     record_source varchar(50) not null
 );
