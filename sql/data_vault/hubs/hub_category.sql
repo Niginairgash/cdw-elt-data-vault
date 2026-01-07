b9de36b424f574415ae8f9ca9d271f02cafe98d9
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-07 11:29:00 +0500

    Update hub_category.sql

--- sql/data_vault/hubs/hub_category.sql
+++ sql/data_vault/hubs/hub_category.sql
@@ -1,6 +1,6 @@
 create table core.hub_category (
     category_hk   char(32) not null,
-    category_id   integer not null,
+    category_bk   integer not null,
     load_dts      timestamp not null,
     record_source varchar(50) not null
 );
