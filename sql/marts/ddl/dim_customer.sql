3066fa425f1783068e61e7cba84871b2bf4a5331
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-12 14:36:25 +0500

    Update dim_customer.sql

--- sql/marts/ddl/dim_customer.sql
+++ sql/marts/ddl/dim_customer.sql
@@ -1,1 +1,13 @@
-
+create table marts.dim_customer(
+    customer_sk   serial primary key,
+    customer_hk   char(32) not null,
+  
+    first_name 		varchar(45),
+	  last_name 		varchar(45),
+	  email 			  varchar(50),
+	  active 			  boolean,
+  
+    start_dts     timestamp not null,
+    end_dts       timestamp,
+    is_current    boolean not null default true
+  );
