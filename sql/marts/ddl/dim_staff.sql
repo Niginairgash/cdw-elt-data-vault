13dd2be3197361fa3d595d4794fdb3fb6088ab19
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-14 15:14:20 +0500

    Update dim_staff.sql

--- sql/marts/ddl/dim_staff.sql
+++ sql/marts/ddl/dim_staff.sql
@@ -1,1 +1,13 @@
+create table marts.dim_staff(
+  staff_sk       serial primary key,
+  staff_hk       char(32) not null,
 
+  first_name 		 varchar(45),
+	last_name 		 varchar(45),
+	email 			   varchar(50),
+	active 			   boolean,
+
+  start_dts      timestamp not null,
+  end_dts        timestamp,
+  is_current     boolean not null default true
+);  
