65fe8a134a3443f7db54be6e7adfb8397b58b5d9
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-14 15:39:19 +0500

    Create dim_film.sql

---
+++ sql/marts/ddl/dim_film.sql
@@ -0,0 +1,16 @@
+create table marts.dim_film(
+  film_sk serial primary key,
+  film_hk char(32) not null,
+
+  title 			varchar(255),
+	release_year 	varchar(4),
+	rental_duration integer,
+	rental_rate 	numeric(4, 2),
+	length 			integer,
+	replacement_cost numeric(5,2),
+	rating 			varchar(20),
+
+  start_dts timestamp not null,
+  end_dts   timestamp,
+  is_current boolean not null default true  
+);
