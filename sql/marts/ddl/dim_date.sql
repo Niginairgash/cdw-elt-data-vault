b1d5b8a45da8bd5a61bf8175a8829d0373b5945b
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-15 14:53:57 +0500

    Create dim_date table for date dimension

--- sql/marts/ddl/dim_date.sql
+++ sql/marts/ddl/dim_date.sql
@@ -1,1 +1,8 @@
-
+create table marts.dim_date(
+  date_pk    int primary key,
+  full_date  date not null,
+  year       int not null,
+  month      int not null,
+  day        int not null,
+  quater     int not null
+  );
