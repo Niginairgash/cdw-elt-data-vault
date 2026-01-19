082b12ccf407ccbb571ae08f55f2ccf3f55c99fd
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-19 16:52:46 +0500

    Update stg_film.sql

---
+++ sql/staging/load/stg_film.sql
@@ -0,0 +1,36 @@
+insert into stage.stg_film(
+  film_id ,
+	title ,
+	description ,
+	release_year ,
+	language_id ,
+	rental_duration ,
+	rental_rate ,
+	length ,
+	replacement_cost ,
+	rating ,
+	last_update ,
+	special_features ,
+	load_dts 	,
+	record_source
+)
+select 
+  film_id ,
+	title ,
+	description ,
+	release_year ,
+	language_id ,
+	rental_duration ,
+	rental_rate ,
+	length ,
+	replacement_cost ,
+	rating ,
+	last_update ,
+	special_features ,
+	now() as load_dts ,
+  'dvdrental' as record_source
+from {{ source }}.film f
+where not exists (
+  select 1 
+  from stage.str_film ss
+  where ss.film_id = f.film_id)
