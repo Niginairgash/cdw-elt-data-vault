6a0666052e20a2477777345095ec334de9b60686
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-19 15:37:12 +0500

    Update stg_actor.sql

--- sql/staging/load/stg_actor.sql
+++ sql/staging/load/stg_actor.sql
@@ -1,4 +1,11 @@
-insert into stage.stg_actor
+insert into stage.stg_actor(
+	actor_id,
+	first_name,
+	last_name,
+	last_update,
+	load_dts,
+	record_source
+)
 select
     actor_id,
 	first_name 		,
@@ -6,4 +13,10 @@ select
 	last_update 	,
 	now() as load_dts ,
     'dvdrental' as record_source
-from {{source}}.actor;
+from {{source}}.actor a
+where not exists(
+	select 1
+	from stage.stg_actor sa
+	where sa.actor_id = a.actor_id
+		and sa.last_update = a.last_update
+);
