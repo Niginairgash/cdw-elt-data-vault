8459b549eff037b5265043d002c8de0c418cf260
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-20 14:52:24 +0500

    Update hub_actor.sql

--- sql/core/load/hubs/hub_actor.sql
+++ sql/core/load/hubs/hub_actor.sql
@@ -1,12 +1,13 @@
-insert into core.hub_actor (actor_key, actor_id, load_dts, record_source)
+insert into core.hub_actor (actor_hk, actor_bk, load_dts, record_source)
 select
-    hashkey(actor_id) as actor_key,
-    actor_id,
+    hashkey(s.actor_id) as actor_hk,
+    s.actor_id as actor_bk,
     now() as load_dts,
     'ods_system' as record_source
 from stage.stg_actor s
 where not exists (  
     select 1 
     from core.hub_actor h 
-    where h.actor_id = hashkey(s.actor_id)
+    where h.actor_hk = hashkey(s.actor_id)
 );
+
