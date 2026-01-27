bb25ec8c30cb952fe2ab245dadc05b88b980aafa
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-27 15:46:26 +0500

    Insert staff-store links into link_staff_store table

--- sql/core/load/links/link_staff_store.sql
+++ sql/core/load/links/link_staff_store.sql
@@ -1,1 +1,14 @@
-
+insert into core.link_staff_store(link_staff_store_hk, staff_hk, store_hk, load_dts, record_source)
+select
+  hashkey(concat(sss.staff_id, '#', ss.store_id)) as link_staff_store_hk,
+  hashkey(sss.staff_id) as staff_hk,
+  hashkey(ss.store_id) as store_hk,
+  now() as load_dts,
+  'dvdrental' as record_source
+from stage.stg_staff sss
+join stage.stg_store ss on sss.store_id = ss.store_id
+where not exists(
+  select 1
+  from core.link_staff_store lss
+  where lss.link_staff_store_hk =  hashkey(concat(sss.staff_id, '#', ss.store_id))   
+);
