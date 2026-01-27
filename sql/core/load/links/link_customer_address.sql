be0c45a31e8d8b9dfe512b391678fca71bac8a66
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-27 11:04:01 +0500

    Update link_customer_address.sql

--- sql/core/load/links/link_customer_address.sql
+++ sql/core/load/links/link_customer_address.sql
@@ -1,1 +1,14 @@
-
+insert into core.link_customer_address(link_customer_address_hk, customer_hk, address_hk, load_dts, record_source)
+select
+  hashkey(concat(sc.customer_id, '#', sa.address_id)) as link_customer_address_hk,
+  hashkey(sc.customer_id) as customer_hk, 
+  hashkey(sa.address_id) as address_hk,
+  now() as load_dts,
+  'dvdrental' as record_source
+from stage.stg_customer sc
+join stage.stg_address sa on sc.address_id = sa.address_id
+where not exists(
+  select 1
+  from core.link_customer_address l
+  where l.link_customer_address_hk = hashkey(concat(sc.customer_id, '#', sa.address_id))
+);
