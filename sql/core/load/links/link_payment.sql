0dc4c701196a5ae9a647061bbb453298f13a6e76
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-27 12:06:49 +0500

    Update link_payment.sql

--- sql/core/load/links/link_payment.sql
+++ sql/core/load/links/link_payment.sql
@@ -1,1 +1,15 @@
-
+insert into core.link_payment(link_payment_hk, customer_hk, staff_hk, link_rental_hk, load_dts, record_source)
+select
+  hashkey(ssp.payment_id)   as link_payment_hk,
+  hashkey(ssp.customer_id)  as customer_hk,
+  hashkey(ssp.staff_id)     as staff_hk,
+  hashkey(ssp.rental_id)    as link_rental_hk,
+  now()                     as load_dts,
+  'dvdrental'               as record_source
+from stage.stg_payment ssp
+where not exists
+(
+  select 1
+  from core.link_payment lp
+  where lp.link_payment_hk = hashkey(ssp.payment_id)
+);
