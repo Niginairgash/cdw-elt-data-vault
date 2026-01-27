aa26c8fe35bf07e98ba436778ecd647a5d8c2844
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-27 15:45:54 +0500

    Update link_staff_store.sql

--- sql/core/ddl/links/link_staff_store.sql
+++ sql/core/ddl/links/link_staff_store.sql
@@ -3,6 +3,6 @@ create table core.link_staff_store(
 	staff_hk 			char(32) not null,
 	store_hk 			char(32) not null,
 	load_dts 			timestamp not null,
-	record_store 		varchar(50) not null,
+	record_source 		varchar(50) not null,
 	primary key (link_staff_store_hk)
 );
