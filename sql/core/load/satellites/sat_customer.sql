5d249b475306b1d10c19f453ba4aa23f1cf44529
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-27 18:00:19 +0500

    Update sat_customer.sql

--- sql/core/load/satellites/sat_customer.sql
+++ sql/core/load/satellites/sat_customer.sql
@@ -1,1 +1,13 @@
-
+insert into core.sat_customer(customer_hk, first_name, last_name, email, active, load_dts, record_source, start_dts, end_dts, is_current)
+select 
+  hashkey(ssc.customer_id) as customer_hk,
+  ssc.first_name as first_name, 
+  ssc.last_name as last_name, 
+  ssc.email as email, 
+  ssc.active as active, 
+  now() as load_dts, 
+  'dvdrental' as record_source, 
+  now() as start_dts, 
+  null as end_dts, 
+  current_timestamp as is_current
+from stage.stg_customer ssc
