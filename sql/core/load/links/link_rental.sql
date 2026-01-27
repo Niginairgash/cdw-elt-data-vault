96cf35c2a7091c1c507f593b36aabd1e6dad9f42
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-27 15:28:22 +0500

    Update link_rental.sql

--- sql/core/load/links/link_rental.sql
+++ sql/core/load/links/link_rental.sql
@@ -1,1 +1,14 @@
-
+insert into core.link_rental(link_rental_hk, customer_hk, inventory_hk, staff_hk, load_dts, record_source)
+select
+  hashkey(ssr.rental_id)     as link_rental_hk,
+  hashkey(ssr.customer_id)   as customer_hk,
+  hashkey(ssr.inventory_id)  as inventory_hk,
+  hashkey(ssr.staff_id)      as staff_hk,
+  now()                      as load_dts,
+  'dvdrental'                as record_source
+from stage.stg_rental ssr
+where not exists (
+  select 1
+  from core.link_rental clr
+  where clr.link_rental_hk = hashkey(ssr.rental_id)
+);
