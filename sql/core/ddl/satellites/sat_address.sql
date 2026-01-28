4592cbea69b193ac3df2cba2a0d633f1d94fd443
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-28 12:03:15 +0500

    Update sat_address.sql

--- sql/core/ddl/satellites/sat_address.sql
+++ sql/core/ddl/satellites/sat_address.sql
@@ -1,16 +1,17 @@
 create table core.sat_address(
 	address_hk 		char(32) not null,
 	
-	address        varchar(50),
-    address2       varchar(50),
-    district       varchar(20),
-    postal_code    varchar(10),
-    phone          varchar(20),
+	address       	varchar(50),
+    address2       	varchar(50),
+    district       	varchar(20),
+    postal_code    	varchar(10),
+    phone          	varchar(20),
     
-    load_dts timestamp not null,
-    record_source varchar(50) not null,
+    load_dts 		timestamp not null,
+    record_source 	varchar(50) not null,
     
-    start_dts timestamp not null,
-    end_dts timestamp,
-    is_current boolean not null default true
+    start_dts 		timestamp not null,
+    end_dts 		timestamp,
+    is_current 		boolean not null default true,
+	hashdiff    	char(32) not null
 );
