f6d2d074d60a63a429b3592ea04c5b87fd94009d
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-28 12:16:57 +0500

    Update sat_customer.sql

--- sql/core/load/satellites/sat_customer.sql
+++ sql/core/load/satellites/sat_customer.sql
@@ -5,14 +5,16 @@ set
 from stage.stg_customer ssc
 where old.customer_hk = hashkey(ssc.customer_id)
   and old.is_current = true
-  and (
-    old.first_name   is distinct from ssc.first_name 
-    or old.last_name is distinct from ssc.last_name 
-    or old.email     is distinct from ssc.email
-    or old.active    is distinct from ssc.active
+  and old.hashdiff <> md5(
+    concat(
+      coalesce(old.first_name,''), '#', 
+      coalesce(old.last_name, ''), '#', 
+      coalesce(old.email, ''),     '#', 
+      coalesce(old.active::text, '')
+    )
 );
 
-insert into core.sat_customer(customer_hk, first_name, last_name, email, active, load_dts, record_source, start_dts, end_dts)
+insert into core.sat_customer(customer_hk, first_name, last_name, email, active, load_dts, record_source, start_dts, end_dts, is_current, hashdiff)
 select 
   hashkey(ssc.customer_id) as customer_hk,
   ssc.first_name as first_name, 
@@ -22,15 +24,25 @@ select
   now() as load_dts, 
   'dvdrental' as record_source, 
   now() as start_dts, 
-  null as end_dts
+  null as end_dts,
+  true as is_current,
+  md5(concat(
+      coalesce(ssc.first_name,''), '#', 
+      coalesce(ssc.last_name, ''), '#', 
+      coalesce(ssc.email, ''),     '#', 
+      coalesce(ssc.active::text, '')
+  ) ) as hashdiff
 from stage.stg_customer ssc
 where not exists(
     select 1
     from core.sat_customer cs
     where cs.customer_hk = hashkey(ssc.customer_id)
       and cs.is_current = true
-      and cs.first_name  is not distinct from ssc.first_name 
-      and cs.last_name   is not distinct from ssc.last_name 
-      and cs.email       is not distinct from ssc.email
-      and cs.active      is not distinct from ssc.active
-  );
+      and cs.hashdiff =   md5(
+          concat(
+            coalesce(ssc.first_name,''), '#', 
+            coalesce(ssc.last_name, ''), '#', 
+            coalesce(ssc.email, ''),     '#', 
+            coalesce(ssc.active::text, '')
+          ))
+    );
