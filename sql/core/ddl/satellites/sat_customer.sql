7c3f8ba6ceda13a37e69e4a5186ef5a46f91aca7
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-28 12:02:25 +0500

    Update sat_customer.sql

--- sql/core/ddl/satellites/sat_customer.sql
+++ sql/core/ddl/satellites/sat_customer.sql
@@ -10,6 +10,7 @@ create table core.sat_customer(
 	record_source 	varchar(50) not null,
 	
 	start_dts 		timestamp not null,
-	end_dts 			timestamp,
-	is_current 		boolean not null default true
+	end_dts 		timestamp,
+	is_current 		boolean not null default true,
+	hashdiff    	char(32) not null
 );
