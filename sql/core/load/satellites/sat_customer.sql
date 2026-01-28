c9e7a2b13d42ca9d7479d21a789236ad70cf31b0
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-01-28 17:00:39 +0500

    Refactor SQL for updating and inserting customer data

--- sql/core/load/satellites/sat_customer.sql
+++ sql/core/load/satellites/sat_customer.sql
@@ -1,48 +1,45 @@
+with src as(
+  select 
+    hashkey(customer_id) as customer_hk,
+    first_name as first_name, 
+    last_name as last_name, 
+    email as email, 
+    active as active, 
+    md5(concat(
+        coalesce(first_name,''), '#', 
+        coalesce(last_name, ''), '#', 
+        coalesce(email, ''),     '#', 
+        coalesce(active::text, '')
+    ) ) as hashdiff
+from stage.stg_customer
+)
 update core.sat_customer old
 set
   end_dts = now(),
   is_current = false
-from stage.stg_customer ssc
-where old.customer_hk = hashkey(ssc.customer_id)
+from src
+where old.customer_hk = src.customer_hk
   and old.is_current = true
-  and old.hashdiff <> md5(
-    concat(
-      coalesce(ssc.first_name,''), '#', 
-      coalesce(ssc.last_name, ''), '#', 
-      coalesce(ssc.email, ''),     '#', 
-      coalesce(ssc.active::text, '')
-    )
-);
+  and old.hashdiff <> src.hashdiff;
 
 insert into core.sat_customer(customer_hk, first_name, last_name, email, active, load_dts, record_source, start_dts, end_dts, is_current, hashdiff)
 select 
-  hashkey(ssc.customer_id) as customer_hk,
-  ssc.first_name as first_name, 
-  ssc.last_name as last_name, 
-  ssc.email as email, 
-  ssc.active as active, 
+  customer_hk,
+  first_name as first_name, 
+  last_name as last_name, 
+  email as email, 
+  active as active, 
   now() as load_dts, 
   'dvdrental' as record_source, 
   now() as start_dts, 
   null as end_dts,
   true as is_current,
-  md5(concat(
-      coalesce(ssc.first_name,''), '#', 
-      coalesce(ssc.last_name, ''), '#', 
-      coalesce(ssc.email, ''),     '#', 
-      coalesce(ssc.active::text, '')
-  ) ) as hashdiff
-from stage.stg_customer ssc
+  hashdiff
+from src
 where not exists(
     select 1
     from core.sat_customer cs
-    where cs.customer_hk = hashkey(ssc.customer_id)
+    where cs.customer_hk = src.customer_hk
       and cs.is_current = true
-      and cs.hashdiff =   md5(
-          concat(
-            coalesce(ssc.first_name,''), '#', 
-            coalesce(ssc.last_name, ''), '#', 
-            coalesce(ssc.email, ''),     '#', 
-            coalesce(ssc.active::text, '')
-          ))
+      and cs.hashdiff =  src.hashdiff
     );
