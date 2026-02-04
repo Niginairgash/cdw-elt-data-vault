13f2b9c279e58a8a41d71ef3a080b62e139b677f
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-04 17:24:25 +0500

    Update load_dag.py

--- dags/stage/load_dag.py
+++ dags/stage/load_dag.py
@@ -14,22 +14,22 @@ def load():
 
 with DAG(
     dag_id = "training_day_6",
-    start_date=datetime(2024,1,1),
-    schedule_interval=None,
+    start_date = datetime(2024,1,1),
+    schedule_interval = None,
 ) as dag:
-    start = EmptyOperator(task_id="start")
+    start = EmptyOperator(task_id = "start")
 
     extract = PythonOperator(
         task_id = "extract",
-        python_callable=extract
+        python_callable = extract
     )
 
     load = PythonOperator(
         task_id = "load",
-        python_callable=load
+        python_callable = load
     )
 
 
-    end = EmptyOperator(task_id="end")
+    end = EmptyOperator(task_id = "end")
 
     start >> extract >> load >> end
