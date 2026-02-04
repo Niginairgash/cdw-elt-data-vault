f7406593f63527325e598552f422bba3f59142dc
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-04 17:23:21 +0500

    Create load_dag.py for Airflow DAG setup

---
+++ dags/stage/load_dag.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+from datetime import datetime
+
+def extract():
+    pass
+
+
+def load():
+    pass
+
+with DAG(
+    dag_id = "training_day_6",
+    start_date=datetime(2024,1,1),
+    schedule_interval=None,
+) as dag:
+    start = EmptyOperator(task_id="start")
+
+    extract = PythonOperator(
+        task_id = "extract",
+        python_callable=extract
+    )
+
+    load = PythonOperator(
+        task_id = "load",
+        python_callable=load
+    )
+
+
+    end = EmptyOperator(task_id="end")
+
+    start >> extract >> load >> end
