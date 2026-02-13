eddd4c9e1e9cc8fc02ae86127551a99689a9f814
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-13 15:31:17 +0500

    Update fact_payment.sql

--- sql/marts/ddl/fact_payment.sql
+++ sql/marts/ddl/fact_payment.sql
@@ -2,6 +2,7 @@ create table marts.fact_payment(
     payment_pk     serial primary key,
     customer_sk    int not null references marts.dim_customer(customer_sk),
     staff_sk       int not null references marts.dim_staff(staff_sk),
+    rental_sk      int not null references marts.dim_rental(rental_sk),
     date_pk        int not null references marts.dim_date(date_pk),
     amount         numeric(5,2) not null,
     payment_cnt 	 int not null
