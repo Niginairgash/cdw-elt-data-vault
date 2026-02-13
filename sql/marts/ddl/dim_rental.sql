f00710ef9e052ca07bc4fb5de22dffeee7775130
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-13 17:49:34 +0500

    Update dim_rental.sql

--- sql/marts/ddl/dim_rental.sql
+++ sql/marts/ddl/dim_rental.sql
@@ -2,9 +2,5 @@ create table marts.dim_rental(
   rental_sk     serial primary key,
   rental_hk     char(32) not null,
   rental_date   timestamp not null,
-  return_date   timestamp,
-  
-  start_dts     timestamp not null,
-  end_dts       timestamp,
-  is_current    boolean not null default true
+  return_date   timestamp
   );
