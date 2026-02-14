c1ef038dc2e719803aba4e29757548222b69afbb
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-14 23:42:29 +0500

    Add SQL script to populate dim_date table
    Insert date records into marts.dim_date with checks for duplicates.

--- sql/marts/load/dim_date.sql
+++ sql/marts/load/dim_date.sql
@@ -1,1 +1,25 @@
-
+insert into marts.dim_date (date_pk, full_date, "year", "month", "day", quater)
+with bounds as(
+	select
+		min(order_date)::date as min_dt,
+		max(order_date)::date as max_dt
+	from public."order" 
+)
+select
+	to_char(d, 'YYYYMMDD')::int 	as date_pk,
+	d::DATE 						as full_date,
+	extract (year from d) 			as "year",
+	extract(month from d) 			as "month",
+	extract(day from d) 			as "day",
+	(extract(month from d)+2) / 3 	as quater	
+from generate_series(
+	(select min_dt from bounds),
+	(select max_dt + interval '1 year' from bounds),
+	interval '1 day'
+	) as d
+-- Check if date already exists to avoid duplicates
+where not exists (
+	select 1
+	from core.dim_date dd
+	where dd.full_date = d::date
+)
