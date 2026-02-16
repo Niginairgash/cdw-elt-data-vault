7c60e0d51a8988e22589437b3494e4aac5da201a
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-16 16:11:11 +0500

    Update dim_rental.sql

--- sql/marts/load/dim_rental.sql
+++ sql/marts/load/dim_rental.sql
@@ -1,1 +1,26 @@
+with src as (
+  select 
+    lr.link_rental_hk as rental_hk,
+    slr.rental_date,
+    slr.return_date
+  from core.link_rental lr
+  join core.sat_link_rental slr
+    on slr.link_rental_hk = lr.link_rental_hk
+)
+  
+update marts.dim_rental dr
+set dr.return_date = src.return_date
+from src
+where dr.rental_hk = src.rental_hk;
 
+insert into marts.dim_rental(rental_hk, rental_date, return_date)
+select 
+  slr.link_rental_hk,
+  slr.rental_date,
+  slr.return_date
+from src
+where not exists(
+  select 1
+  from marts.dim_rental dr
+  where dr.return_date = src.return_date
+);
