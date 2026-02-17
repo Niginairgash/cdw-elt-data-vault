df62138202fa226ba24091695e1c4dbe418db0d6
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-17 13:30:51 +0500

    Add business key - payment_hk column

--- sql/marts/ddl/fact_payment.sql
+++ sql/marts/ddl/fact_payment.sql
@@ -1,9 +1,10 @@
 create table marts.fact_payment(
     payment_pk     serial primary key,
+    payment_hk     char(32) not null unique, 
     customer_sk    int not null references marts.dim_customer(customer_sk),
     staff_sk       int not null references marts.dim_staff(staff_sk),
     rental_sk      int not null references marts.dim_rental(rental_sk),
     date_pk        int not null references marts.dim_date(date_pk),
     amount         numeric(5,2) not null,
-    payment_cnt 	 int not null
+    payment_cnt    int not null
   );
