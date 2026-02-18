5ac8998247f8195c1e77b982ba3f12bd54b4073c
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-18 17:56:10 +0500

    Update fact_rental.sql

--- sql/marts/ddl/fact_rental.sql
+++ sql/marts/ddl/fact_rental.sql
@@ -1,10 +1,12 @@
 create table marts.fact_rental(
   rental_pk     serial primary key,
+  rental_hk     char(32) not null,
   customer_sk   int not null references marts.dim_customer(customer_sk),
   staff_sk      int not null references marts.dim_staff(staff_sk),
   film_sk       int not null references marts.dim_film(film_sk),
   date_pk       int not null references marts.dim_date(date_pk),
   rental_status varchar(20) not null,
   rental_cnt    int not null,
-  amount        numeric(10,2)
+  amount        numeric(10,2),
+  unique (rental_hk)
 );
