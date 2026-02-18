569ff42a8d7a0b3e3d0653847c3c38f97c9e8abf
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-18 17:31:34 +0500

    Update dim_film.sql

--- sql/marts/ddl/dim_film.sql
+++ sql/marts/ddl/dim_film.sql
@@ -1,16 +1,18 @@
 create table marts.dim_film(
-  film_sk serial primary key,
-  film_hk char(32) not null,
+  film_sk 			serial primary key,
+  film_hk 			char(32) not null,
 
   title 			varchar(255),
-	release_year 	varchar(4),
-	rental_duration integer,
-	rental_rate 	numeric(4, 2),
-	length 			integer,
-	replacement_cost numeric(5,2),
-	rating 			varchar(20),
+  release_year 		varchar(4),
+  rental_duration 	integer,
+  rental_rate 		numeric(4, 2),
+  length 			integer,
+  replacement_cost 	numeric(5,2),
+  rating 			varchar(20),
 
-  start_dts timestamp not null,
-  end_dts   timestamp,
-  is_current boolean not null default true  
+  hashdif		  	char(32) not null,
+	
+  start_dts 	  	timestamp not null,
+  end_dts   	  	timestamp,
+  is_current 	  	boolean not null default true  
 );
