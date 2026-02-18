3bfe5bac044bdcd32facdcc6b2c5ffc42da87341
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-18 10:45:40 +0500

    Update dim_staff.sql

--- sql/marts/load/dim_staff.sql
+++ sql/marts/load/dim_staff.sql
@@ -1,1 +1,26 @@
-
+insert into marts.dim_staff(
+  staff_hk,
+  first_name,
+	last_name,
+	email,
+	active,
+  start_dts,
+  end_dts,
+  is_current
+)
+select
+  ss.staff_hk,
+  ss.first_name,
+	ss.last_name,
+	ss.email,
+	ss.active,
+  ss.start_dts,
+  ss.end_dts,
+  ss.is_current
+from core.sat_staff ss
+where not exists(
+  select 1
+  from marts.dim_staff ds
+  where ss.staff_hk = ds.staff_hk
+    and ss.start_dts = ds.start_dts
+)
