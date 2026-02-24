2d9aec9a388a4e97765c35e7358c1159da0a9093
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-24 15:15:50 +0500

    Update dag_stage_customer.py

--- dags/stage/dag_stage_customer.py
+++ dags/stage/dag_stage_customer.py
@@ -1,4 +1,27 @@
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
+from airflow.utils.task_group import TaskGroup
 from datetime import datetime 
+
+
+with DAG (
+  dag_id = "dag_stage_customer",
+  start_date = datetime(2024,1,1),
+  schedule_interval = '@daily',
+  catchup = False
+) as dag:
+
+  start = EmptyOperator(task_id = "start")
+  
+  with TaskGroup("stg_customer") as stg_group:
+    stg_ddl = PythonOperator(task_id = 'stage_ddl')
+      
+    )
+  end = EmptyOperator(task_id = "end")
+  start >> end
+
+
+
+
+  
