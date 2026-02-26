faeae7e889277e093954bd33fbd27e4141d7aa0e
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 11:34:31 +0500

    Change address_hk type from char to UUID

--- sql/core/ddl/hubs/hub_address.sql
+++ sql/core/ddl/hubs/hub_address.sql
@@ -1,5 +1,5 @@
 create table core.hub_address(
-	address_hk 		char(32) not null,
+	address_hk 		UUID not null,
 	address_bk 		text not null,
 	load_dts 		timestamp not null,
 	record_source 	varchar(50) not null,
