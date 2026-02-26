94d8eaf2d6718a3ebe239256e9eee00c625bc648
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 11:26:49 +0500

    Update link_customer_address.sql

--- sql/core/ddl/links/link_customer_address.sql
+++ sql/core/ddl/links/link_customer_address.sql
@@ -1,7 +1,7 @@
 create table core.link_customer_address(
-	link_customer_address_hk 	char(32) not null,
-	customer_hk 				char(32) not null,
-	address_hk 					char(32) not null,
+	link_customer_address_hk 	UUID not null,
+	customer_hk 				UUID not null,
+	address_hk 					UUID not null,
 	load_dts 					timestamp not null,
 	record_source 				varchar(50) not null,
 	primary key(link_customer_address_hk)
