b4ded79d6f5249eb81c80d04c9351647ec48b309
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 11:32:06 +0500

    Change link rental keys from char to UUID

--- sql/core/ddl/links/link_rental.sql
+++ sql/core/ddl/links/link_rental.sql
@@ -1,8 +1,8 @@
 create table core.link_rental(
-	link_rental_hk 	char(32) not null,
-    customer_hk 	char(32) not null,
-    inventory_hk 	char(32) not null,
-    staff_hk 		char(32) not null,
+	link_rental_hk 	UUID not null,
+    customer_hk 	UUID not null,
+    inventory_hk 	UUID not null,
+    staff_hk 		UUID not null,
 	load_dts  		timestamp not null,
 	record_source 	varchar(50) not null,
 	primary key (link_rental_hk)
