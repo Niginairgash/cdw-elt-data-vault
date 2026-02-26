6d5faadc3ce04e35717844574b4c9c479606a41f
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 11:35:59 +0500

    Change store_hk type from char to UUID

--- sql/core/ddl/hubs/hub_store.sql
+++ sql/core/ddl/hubs/hub_store.sql
@@ -1,5 +1,5 @@
 create table core.hub_store (
-    store_hk        char(32) not null,
+    store_hk        UUID not null,
     store_bk        text not null,
     load_dts        timestamp not null,
     record_source   varchar(50) not null,
