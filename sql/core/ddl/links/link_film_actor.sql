ffa77631b7aa9508665b18ee429be4f87e65676e
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 11:30:45 +0500

    Update link_film_actor.sql

--- sql/core/ddl/links/link_film_actor.sql
+++ sql/core/ddl/links/link_film_actor.sql
@@ -1,7 +1,7 @@
 create table core.link_film_actor(
-	link_film_actor_hk 	char(32) not null,
-	film_hk 			char(32) not null,
-	actor_hk 			char(32) not null,	
+	link_film_actor_hk 	UUID not null,
+	film_hk 			UUID not null,
+	actor_hk 			UUID not null,	
 	load_dts  		timestamp not null,
 	record_source 	varchar(50) not null,
 	primary key (link_film_actor_hk)
