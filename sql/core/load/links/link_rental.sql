52b51c7e1154a67715c6216070a3812086627394
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 13:03:33 +0500

    Change hashkey to MD5 for link rental fields

--- sql/core/load/links/link_rental.sql
+++ sql/core/load/links/link_rental.sql
@@ -1,11 +1,11 @@
 insert into core.link_rental(link_rental_hk, customer_hk, inventory_hk, staff_hk, load_dts, record_source)
 select
-  hashkey(ssr.rental_id)     as link_rental_hk,
-  hashkey(ssr.customer_id)   as customer_hk,
-  hashkey(ssr.inventory_id)  as inventory_hk,
-  hashkey(ssr.staff_id)      as staff_hk,
-  now()                      as load_dts,
-  'dvdrental'                as record_source
+  MD5(ssr.rental_id)     as link_rental_hk,
+  MD5(ssr.customer_id)   as customer_hk,
+  MD5(ssr.inventory_id)  as inventory_hk,
+  MD5(ssr.staff_id)      as staff_hk,
+  now()                  as load_dts,
+  'dvdrental'            as record_source
 from stage.stg_rental ssr
 where not exists (
   select 1
