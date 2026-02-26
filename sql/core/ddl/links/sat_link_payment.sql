da3df71c17679884c47c0eaf46ce8e0ab2eb4ed4
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 11:33:27 +0500

    Change link_payment_hk type from char to UUID

--- sql/core/ddl/links/sat_link_payment.sql
+++ sql/core/ddl/links/sat_link_payment.sql
@@ -1,7 +1,7 @@
 create table core.sat_link_payment(
-	link_payment_hk 	char(32) not null,
-	amount 				    numeric(5,2) not null,
-	payment_date 		  timestamp not null,
-	load_dts  			  timestamp not null,
+	link_payment_hk 	UUID not null,
+	amount 				numeric(5,2) not null,
+	payment_date 		timestamp not null,
+	load_dts  			timestamp not null,
 	record_source 		varchar(50) not null
 );
