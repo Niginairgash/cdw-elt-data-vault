59554e6611ac82181dcc86a759c0dec986b402f5
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 13:04:39 +0500

    Update link_staff_store.sql

--- sql/core/load/links/link_staff_store.sql
+++ sql/core/load/links/link_staff_store.sql
@@ -1,10 +1,10 @@
 insert into core.link_staff_store(link_staff_store_hk, staff_hk, store_hk, load_dts, record_source)
 select
-  hashkey(concat(sss.staff_id, '#', ss.store_id)) as link_staff_store_hk,
-  hashkey(sss.staff_id) as staff_hk,
-  hashkey(ss.store_id) as store_hk,
-  now() as load_dts,
-  'dvdrental' as record_source
+  MD5(concat(sss.staff_id, '#', ss.store_id)) as link_staff_store_hk,
+  MD5(sss.staff_id)  as staff_hk,
+  MD5(ss.store_id)   as store_hk,
+  now()              as load_dts,
+  'dvdrental'        as record_source
 from stage.stg_staff sss
 join stage.stg_store ss on sss.store_id = ss.store_id
 where not exists(
