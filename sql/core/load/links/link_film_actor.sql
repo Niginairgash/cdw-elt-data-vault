2f24fbdefe3a4b9ccbd69981429e07f46ace0497
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 11:57:00 +0500

    Update link_film_actor.sql

--- sql/core/load/links/link_film_actor.sql
+++ sql/core/load/links/link_film_actor.sql
@@ -1,8 +1,8 @@
 insert into core.link_film_actor (link_film_actor_hk, film_hk, actor_hk, load_dts, record_source)
 select
-    hashkey(concat(s.film_id, '#', s.actor_id)) as link_film_actor_hk,
-    hashkey(s.film_id) as film_hk,
-    hashkey(s.actor_id) as actor_hk,
+    MD5(concat(s.film_id, '#', s.actor_id)) as link_film_actor_hk,
+    MD5(s.film_id) as film_hk,
+    MD5(s.actor_id) as actor_hk,
     now() as load_dts,
     'dvdrental' as record_source
 from stage.stg_film_actor s
