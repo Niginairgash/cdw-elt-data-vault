4d4ce0fb262ce75a3a8ae9337faffc5dad2bf011
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 11:35:03 +0500

    Change customer_hk type from char to UUID

--- sql/core/ddl/hubs/hub_customer.sql
+++ sql/core/ddl/hubs/hub_customer.sql
@@ -1,5 +1,5 @@
 create table core.hub_customer (
-    customer_hk   char(32) not null,
+    customer_hk   UUID not null,
     customer_bk   text not null,
     load_dts      timestamp not null,
     record_source varchar(50) not null,
