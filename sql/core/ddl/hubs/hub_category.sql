504b24376526a1c279b1eac43e5e85f87d8955a6
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 11:34:46 +0500

    Change category_hk type from char to UUID

--- sql/core/ddl/hubs/hub_category.sql
+++ sql/core/ddl/hubs/hub_category.sql
@@ -1,5 +1,5 @@
 create table core.hub_category (
-    category_hk   char(32) not null,
+    category_hk   UUID not null,
     category_bk   text not null,
     load_dts      timestamp not null,
     record_source varchar(50) not null,
