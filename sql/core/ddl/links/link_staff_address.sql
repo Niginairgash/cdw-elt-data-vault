6f5ea8b58cba20f68e683732a59ed25fddc18b5a
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 11:32:31 +0500

    Update link_staff_address.sql

--- sql/core/ddl/links/link_staff_address.sql
+++ sql/core/ddl/links/link_staff_address.sql
@@ -1,7 +1,7 @@
 create table core.link_staff_address(
-	link_staff_address_hk 	char(32) not null,
-	staff_hk 				char(32) not null,
-	address_hk 				char(32) not null,
+	link_staff_address_hk 	UUID not null,
+	staff_hk 				UUID not null,
+	address_hk 				UUID not null,
 	load_dts 				timestamp not null,
 	record_source 			varchar(50) not null,
 	primary key(link_staff_address_hk)
