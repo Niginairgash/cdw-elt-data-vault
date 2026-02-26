bd66df7a5618742c0ee90084e7564f8c66910191
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 11:35:24 +0500

    Change film_hk type from char to UUID

--- sql/core/ddl/hubs/hub_film.sql
+++ sql/core/ddl/hubs/hub_film.sql
@@ -1,5 +1,5 @@
 create table core.hub_film (
-    film_hk       char(32) not null,
+    film_hk       UUID not null,
     film_bk       text not null,
     load_dts      timestamp not null,
     record_source varchar(50) not null,
