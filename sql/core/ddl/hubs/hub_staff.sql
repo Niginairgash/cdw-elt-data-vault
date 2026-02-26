1f8f05ac77abd60ac6619d2c987922a8fcabf227
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 11:35:45 +0500

    Change staff_hk type from char to UUID

--- sql/core/ddl/hubs/hub_staff.sql
+++ sql/core/ddl/hubs/hub_staff.sql
@@ -1,5 +1,5 @@
 create table core.hub_staff (
-    staff_hk        char(32) not null,
+    staff_hk        UUID not null,
     staff_bk        text not null,
     load_dts        timestamp not null,
     record_source   varchar(50) not null,
