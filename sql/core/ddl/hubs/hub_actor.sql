7e3f70fb1027bf28ebb577f787fc585f51cb3f54
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 11:34:08 +0500

    Change actor_hk type from char to UUID

--- sql/core/ddl/hubs/hub_actor.sql
+++ sql/core/ddl/hubs/hub_actor.sql
@@ -1,5 +1,5 @@
 create table core.hub_actor (
-    actor_hk        char(32) not null,
+    actor_hk        UUID not null,
     actor_bk        text not null,
     load_dts        timestamp not null,
     record_source   varchar(50) not null,
