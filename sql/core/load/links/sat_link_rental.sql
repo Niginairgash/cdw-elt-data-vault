c318d85c444ed7f4e3698f54e5ab5c59531215d9
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 13:05:55 +0500

    Change hashkey to MD5 for link rental hash

--- sql/core/load/links/sat_link_rental.sql
+++ sql/core/load/links/sat_link_rental.sql
@@ -1,10 +1,10 @@
 insert into core.sat_link_rental(link_rental_hk, rental_date, return_date, load_dts, record_source)
 select 
-  hashkey(ssr.rental_id) as link_rental_hk,
-  ssr.rental_date as rental_date,
-  ssr.return_date as return_date,
-  now() as load_dts,
-  'dvdrental' as record_source
+  MD5(ssr.rental_id) as link_rental_hk,
+  ssr.rental_date    as rental_date,
+  ssr.return_date    as return_date,
+  now()              as load_dts,
+  'dvdrental'        as record_source
 from stage.stg_rental ssr
 where not exists(
   select 1
