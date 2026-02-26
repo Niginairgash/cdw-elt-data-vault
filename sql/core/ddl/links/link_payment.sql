dee309c8deeefc8aec342585c11e3f6a18887a60
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 11:31:41 +0500

    Change link_payment columns to UUID type

--- sql/core/ddl/links/link_payment.sql
+++ sql/core/ddl/links/link_payment.sql
@@ -1,8 +1,8 @@
 create table core.link_payment(
-	link_payment_hk char(32) not null,
-	customer_hk 	char(32) not null,
-	staff_hk 		char(32) not null,
-	link_rental_hk	char(32) not null,
+	link_payment_hk UUID not null,
+	customer_hk 	UUID not null,
+	staff_hk 		UUID not null,
+	link_rental_hk	UUID not null,
 	load_dts  		timestamp not null,
 	record_source 	varchar(50) not null,
 	primary key (link_payment_hk)
