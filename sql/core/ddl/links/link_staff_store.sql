2d669c7f03944b4bd1c0c3edbf4d00809ef258c1
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 11:32:54 +0500

    Change link_staff_store columns to UUID type

--- sql/core/ddl/links/link_staff_store.sql
+++ sql/core/ddl/links/link_staff_store.sql
@@ -1,7 +1,7 @@
 create table core.link_staff_store(
-	link_staff_store_hk char(32) not null,
-	staff_hk 			char(32) not null,
-	store_hk 			char(32) not null,
+	link_staff_store_hk UUID not null,
+	staff_hk 			UUID not null,
+	store_hk 			UUID not null,
 	load_dts 			timestamp not null,
 	record_source 		varchar(50) not null,
 	primary key (link_staff_store_hk)
