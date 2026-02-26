7026a9b40b5b54c5d2b9eff3d215c55e397c2350
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2026-02-26 14:20:34 +0500

    Update link_customer_address.sql

--- sql/core/load/links/link_customer_address.sql
+++ sql/core/load/links/link_customer_address.sql
@@ -1,14 +1,13 @@
 insert into core.link_customer_address(link_customer_address_hk, customer_hk, address_hk, load_dts, record_source)
 select
-  MD5(concat(sc.customer_id, '#', sa.address_id)) as link_customer_address_hk,
+  MD5(concat(sc.customer_id, '#', sc.address_id)) as link_customer_address_hk,
   MD5(sc.customer_id) as customer_hk, 
-  MD5(sa.address_id) as address_hk,
+  MD5(sc.address_id) as address_hk,
   now() as load_dts,
   'dvdrental' as record_source
 from stage.stg_customer sc
-join stage.stg_address sa on sc.address_id = sa.address_id
 where not exists(
   select 1
   from core.link_customer_address l
-  where l.link_customer_address_hk = hashkey(concat(sc.customer_id, '#', sa.address_id))
+  where l.link_customer_address_hk = hashkey(concat(sc.customer_id, '#', sc.address_id))
 );
